e4bc75f909423ef17af5572d6027550d1ed1b0e9
Mukesh Kaushal <imukeshkaushal@gmail.com>
2023-01-18 15:30:44 +0530

    Admin Section Added

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -1,1 +1,21 @@
 // Store Code Here
+
+import {
+    applyMiddleware,
+    combineReducers,
+    compose,
+    legacy_createStore,
+  } from "redux";
+  
+import thunk from "redux-thunk";
+import { adminReducer } from "./admin/admin.reducer";
+
+  
+  const composeInhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+  const rootReducer = combineReducers({
+        adminProductsManager : adminReducer,
+  });
+  export const store = legacy_createStore(
+    rootReducer,
+    composeInhancer(applyMiddleware(thunk))
+  );
