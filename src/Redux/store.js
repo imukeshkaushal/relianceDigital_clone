c3394c9e24df9100c4e7758a0959815b3787d06f
Tushit99 <97578587+Tushit99@users.noreply.github.com>
2023-01-18 18:29:56 +0530

    cart-page

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -9,11 +9,13 @@ import {
   
 import thunk from "redux-thunk";
 import { adminReducer } from "./admin/admin.reducer";
+import { cartReducer } from "./cart/cart.reducer";
 
   
   const composeInhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
   const rootReducer = combineReducers({
         adminProductsManager : adminReducer,
+        cartManager : cartReducer
   });
   export const store = legacy_createStore(
     rootReducer,
