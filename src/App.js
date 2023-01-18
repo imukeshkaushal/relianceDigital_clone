98ca6234e74a57f684819bb2b55e13aa5bf12c27
Mukesh Kaushal <imukeshkaushal@gmail.com>
2023-01-19 00:33:59 +0530

    Changes Reflected

--- src/App.js
+++ src/App.js
@@ -1,21 +1,4 @@
 
-import "./App.css";
-
-function App() {
-  return <div className="App"></div>;
-
-
-import Admin from './Admin/Admin';
-import './App.css';
-import AdminLogin from './Pages/AdminLogin';
-import Login from './Pages/Login';
-import Register from './Pages/Register';
-
-import Footer from './Components/Footer';
-import Navbar from './Components/Navbar';
-import Carousel1 from './Pages/Carousel';
-
-
 function App() {
   return (
     <div>
