b8be0a88b377ef0daba9a1b09270e82e03e7bdef
Mukesh Kaushal <imukeshkaushal@gmail.com>
2023-01-19 17:39:47 +0530

    Login/Signup Added

--- src/Components/LoginForm.jsx
+++ src/Components/LoginForm.jsx
@@ -1,11 +1,82 @@
-import { Flex, FormControl, FormLabel,  Input, Spacer, Text } from "@chakra-ui/react";
-import React from "react";
-import { Link } from "react-router-dom";
+import { CheckCircleIcon, WarningIcon } from "@chakra-ui/icons";
+import {
+  Flex,
+  FormControl,
+  FormLabel,
+  Input,
+  Spacer,
+  Text,
+  useToast,
+} from "@chakra-ui/react";
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Link, useNavigate } from "react-router-dom";
+import { userLogin } from "../Redux/auth/auth.action";
+
+let intdata = {
+  email: "",
+  password: "",
+};
 
 const LoginForm = () => {
+  const [data, setData] = useState(intdata);
+  const existingData = useSelector((state) => state.authManager.userdata);
+  const dispatch = useDispatch();
+  const toast = useToast();
+  const isAuth = useSelector((state) => state.authManager.isAuth);
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (
+      data.email !== existingData.email ||
+      data.password !== existingData.password
+    ) {
+      toast({
+        position: "bottom-center",
+
+        render: () => (
+          <Flex color="white" borderRadius={"15px"} p={"10px"} bgColor="red" alignItems={"center"}>
+            <WarningIcon w={30} h={30} />
+            <Text size="lg" ml="15px">
+              Ohh ! Sorry, You entered Wrong Email or Password. Please Enter Correct Details. Try Again Login!
+            </Text>
+          </Flex>
+        ),
+      });
+    } else {
+      toast({
+        position: "bottom-center",
+        render: () => (
+          <Flex
+            color="white"
+            borderRadius={"15px"}
+            p={"10px"}
+            bgColor="green.400"
+            alignItems={"center"}
+          >
+            <CheckCircleIcon w={30} h={30} />
+            <Text size="lg" ml="15px">
+              You are Successfully Login. We redirected you to HomePage.
+            </Text>
+          </Flex>
+        ),
+      });
+      dispatch(userLogin());
+      navigate("/");
+    }
+
+    setData(intdata);
+  };
+  const handleChange = (e) => {
+    const { type, value } = e.target;
+    setData({ ...data, [type]: value });
+  };
+  
+
   return (
     <div>
-      <form>
+      <form onSubmit={handleSubmit}>
         <FormControl
           isRequired
           mt={2}
@@ -15,13 +86,10 @@ const LoginForm = () => {
           pt={6}
         >
           <FormLabel>Email Address</FormLabel>
-          <Input
-            placeholder="Enter Email Address"
-            borderRadius={"none"}
-          />
+          <Input placeholder="Enter Email Address" type="email" borderRadius={"none"} value={data.email} onChange={handleChange}/>
 
           <FormLabel mt={8}>Password</FormLabel>
-          <Input placeholder="Enter Password Here" borderRadius="none" />
+          <Input placeholder="Enter Password Here" borderRadius="none" type="password" value={data.password} onChange={handleChange}/>
 
           <Flex mt={4} pl={2} pr={2}>
             <Link to="/admin/login">
