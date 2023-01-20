2e066890002ce27deeed1a8cb3669b259ba5213f
Abha Meshram <abha25meshram@gmail.com>
2023-01-20 23:05:37 +0530

    add footer large size

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 
+import { useEffect, useState } from "react";
 import Footer from "./Components/Footer";
+import Loader from "./Components/Loader";
 import Navbar from "./Components/Navbar";
 import AllRoutes from "./Routers/AllRoutes";
 
 function App() {
-    return (
+  const [isLoading, setIsLoading] = useState(true);
+  useEffect(() => {
+      setIsLoading(true);
+      let timer = setTimeout(() => {
+          setIsLoading(false);
+          return () => clearInterval(timer);
+      }, 1000);
+  }, []);
+    return isLoading?(
+      <Loader/>
+    ):(
     <>
-    <Navbar />   
+      <Navbar />   
       <AllRoutes />
       <Footer/>
     </>
