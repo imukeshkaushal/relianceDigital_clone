ded8a5f40924d624b3412954fba30c74ffd524e8
Mukesh Kaushal <imukeshkaushal@gmail.com>
2023-01-21 11:10:00 +0530

    Error Fixed

--- src/Redux/Products/ProductApi.js
+++ src/Redux/Products/ProductApi.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 let el = JSON.parse(localStorage.getItem("items"))
-el = el.join("")
+
 console.log(el)
 
 export const getPostsApi = async() => {
--- src/Routers/AllRoutes.jsx
+++ src/Routers/AllRoutes.jsx
@@ -3,12 +3,12 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Home from "../Pages/Home/Home";
 
-import React from 'react'
-import { Route, Routes } from 'react-router-dom'
+
+
 import Admin from '../Admin/Admin'
 import AdminRequireAuth from "../Admin/AdminRequireAuth"
 import AdminLogin from '../Pages/AdminLogin'
-import Home from '../Pages/Home'
+
 import Login from '../Pages/Login'
 import Register from '../Pages/Register'
 
