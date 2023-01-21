2f64f7eab15ed84a1c2820fc596fe4bd04c9c99b
Mukesh Kaushal <imukeshkaushal@gmail.com>
2023-01-21 11:21:40 +0530

    issue fixed

--- src/Routers/AllRoutes.jsx
+++ src/Routers/AllRoutes.jsx
@@ -1,34 +1,31 @@
-
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Home from "../Pages/Home/Home";
 
+import Admin from "../Admin/Admin";
+import AdminRequireAuth from "../Admin/AdminRequireAuth";
+import AdminLogin from "../Pages/AdminLogin";
 
-
-import Admin from '../Admin/Admin'
-import AdminRequireAuth from "../Admin/AdminRequireAuth"
-import AdminLogin from '../Pages/AdminLogin'
-
-import Login from '../Pages/Login'
-import Register from '../Pages/Register'
-
+import Login from "../Pages/Login";
+import Register from "../Pages/Register";
 
 const AllRoutes = () => {
   return (
     <div>
-
       <Routes>
         <Route path="/" element={<Home />}></Route>
+        <Route
+          path="/admin"
+          element={
+            <AdminRequireAuth>
+              <Admin />
+            </AdminRequireAuth>
+          }
+        ></Route>
+        <Route path="/admin/login" element={<AdminLogin />}></Route>
+        <Route path="/login" element={<Login />}></Route>
+        <Route path="/register" element={<Register />}></Route>
       </Routes>
-
-        <Routes>
-            <Route path='/' element = {<Home/>}></Route>
-            <Route path='/admin' element = {<AdminRequireAuth><Admin/></AdminRequireAuth>}></Route>
-            <Route path='/admin/login' element = {<AdminLogin/>}></Route>
-            <Route path='/login' element={<Login/>}></Route>
-            <Route path='/register' element={<Register/>}></Route>
-        </Routes>
-
     </div>
   );
 };
