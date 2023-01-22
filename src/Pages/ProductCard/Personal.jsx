4cf409765614539b90dfc72053e84c269acc221a
unknown <vermabraj>
2023-01-22 17:33:21 +0530

    added all skeleton

--- src/Pages/ProductCard/Personal.jsx
+++ src/Pages/ProductCard/Personal.jsx
@@ -1,8 +1,10 @@
 import { useSelector, useDispatch } from "react-redux";
 import { store } from "../../Redux/store";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { getPosts, getPostsMobile, getPostsPersonal, getPostsTelevisions } from "../../Redux/Products/Product.action";
-
+import Skeleton from 'react-loading-skeleton'
+import 'react-loading-skeleton/dist/skeleton.css'
+import { addItemCart } from "../../Redux/cart/cart.action";
 import {
   Card,
   CardBody,
@@ -29,11 +31,19 @@ import Carousel from "./carousel";
 const Personal = () => {
   const { loading, error, data } = useSelector((store) => store.product);
 
+  
+  const [skeletonLoading,setSkeletonLoading] = useState(true)
+
   const dispatch = useDispatch();
   //console.log(data);
   useEffect(() => {
-    dispatch(getPostsPersonal());
+    setTimeout(() => {
+      dispatch(getPostsPersonal());
+      setSkeletonLoading(false)
+    }, 5000);
+   
   }, []);
+ 
 
   const sortByLH = () => {
     console.log("LH",data);
@@ -91,107 +101,205 @@ const Personal = () => {
           <button onClick={sortByLH} id="sortingButton" >Low to High</button>
           <button onClick={sortByHL} id="sortingButton">High to Low</button>
         </div>
-          </div>
+        </div>
           
+        {skeletonLoading  ? 
+          
+       ( <div id="productCards">
+       <div>
+       <Skeleton highlightColor="" height={140} />
+     <h1><Skeleton/></h1>
+     <h1><Skeleton/></h1>
+     <p><Skeleton/></p>
+     <h1><Skeleton height={50}/></h1>
+     </div>
+     <div>
+     <Skeleton height={140} />
+   <h1><Skeleton/></h1>
+   <h1><Skeleton/></h1>
+   <p><Skeleton/></p>
+   <h1><Skeleton height={50}/></h1>
+   </div>
+   <div>
+   <Skeleton height={140} />
+ <h1><Skeleton/></h1>
+ <h1><Skeleton/></h1>
+ <p><Skeleton/></p>
+ <h1><Skeleton height={50}/></h1>
+ </div>
+ <div>
+ <Skeleton height={140} />
+<h1><Skeleton/></h1>
+<h1><Skeleton/></h1>
+<p><Skeleton/></p>
+<h1><Skeleton height={50}/></h1>
+</div>
+<div>
+<Skeleton height={140} />
+<h1><Skeleton/></h1>
+<h1><Skeleton/></h1>
+<p><Skeleton/></p>
+<h1><Skeleton height={50}/></h1>
+</div>
+<div>
+<Skeleton height={140} />
+<h1><Skeleton/></h1>
+<h1><Skeleton/></h1>
+<p><Skeleton/></p>
+<h1><Skeleton height={50}/></h1>
+</div>
+<div>
+<Skeleton height={140} />
+<h1><Skeleton/></h1>
+<h1><Skeleton/></h1>
+<p><Skeleton/></p>
+<h1><Skeleton height={50}/></h1>
+</div>
+<div>
+<Skeleton height={140} />
+<h1><Skeleton/></h1>
+<h1><Skeleton/></h1>
+<p><Skeleton/></p>
+<h1><Skeleton height={50}/></h1>
+</div>
+<div>
+<Skeleton height={140} />
+<h1><Skeleton/></h1>
+<h1><Skeleton/></h1>
+<p><Skeleton/></p>
+<h1><Skeleton height={50}/></h1>
+</div>
+<div>
+<Skeleton height={140} />
+<h1><Skeleton/></h1>
+<h1><Skeleton/></h1>
+<p><Skeleton/></p>
+<h1><Skeleton height={50}/></h1>
+</div>
+<div>
+<Skeleton height={140} />
+<h1><Skeleton/></h1>
+<h1><Skeleton/></h1>
+<p><Skeleton/></p>
+<h1><Skeleton height={50}/></h1>
+</div>
+<div>
+<Skeleton height={140} />
+<h1><Skeleton/></h1>
+<h1><Skeleton/></h1>
+<p><Skeleton/></p>
+<h1><Skeleton height={50}/></h1>
+</div>
+     </div>)
+        :
+        
+        (<div id="productCards">
+          {data.map((product) => (
+            
+            <Card  variant="outline" key={product.id} id="Productcard" _hover={{boxShadow: "rgba(255, 255, 255, 0.1) 0px 1px 1px 0px inset, rgba(50, 50, 93, 0.25) 0px 50px 100px -20px, rgba(0, 0, 0, 0.3) 0px 30px 60px -30px"}}>
+              <CardBody id="productBody">
+                <Image
+                  mt="-5"
+                  src={product.img}
+                  alt="Product image"
+                  borderRadius="lg"
+                />
 
-          <div id="productCards"
-          >
-            {data.map((product) => (
-              <Card  variant="outline" key={product.id} id="Productcard" _hover={{boxShadow: "rgba(255, 255, 255, 0.1) 0px 1px 1px 0px inset, rgba(50, 50, 93, 0.25) 0px 50px 100px -20px, rgba(0, 0, 0, 0.3) 0px 30px 60px -30px"}}>
-                <CardBody id="productBody">
-                  <Image
-                    mt="-5"
-                    src={product.img}
-                    alt="Product image"
-                    borderRadius="lg"
-                  />
-
-                  <Stack spacing="1">
+                <Stack spacing="1">
+                  <Text
+                    color="blue.700"
+                    fontSize={"medium"}
+                    _hover={{ color: "red" }}
+                    id="cardTitle"
+                  >
+                    {product.name}
+                  </Text>
+                  <Stack direction="row">
                     <Text
-                      color="blue.700"
-                      fontSize={"medium"}
-                      _hover={{ color: "red" }}
-                      id="cardTitle"
+                      color="gray.600"
+                      fontSize="smaller"
+                      fontWeight={"bold"}
                     >
-                      {product.name}
+                      {" "}
+                      <span>&#8377;</span>
+                      {product.mrp}
                     </Text>
-                    <Stack direction="row">
-                      <Text
-                        color="gray.600"
-                        fontSize="smaller"
-                        fontWeight={"bold"}
-                      >
-                        {" "}
+                    <Text
+                      color="gray.500"
+                      fontSize="smaller"
+                      fontWeight={"bold"}
+                    >
+                      <s>
                         <span>&#8377;</span>
-                        {product.mrp}
-                      </Text>
-                      <Text
-                        color="gray.500"
-                        fontSize="smaller"
-                        fontWeight={"bold"}
-                      >
-                        <s>
-                          <span>&#8377;</span>
-                          {product.mrp}{" "}
-                        </s>{" "}
-                      </Text>
-                      <Text
-                        color="green.600"
-                        fontSize="x-small"
-                        fontWeight={"bold"}
-                      >
-                        {" "}
-                        <span>&#8377;</span>
-                        {product.discount}
-                      </Text>
-                    </Stack>
-                    <Stack direction="row">
-                      <Badge
-                        variant="outline"
-                        colorScheme="green"
-                        borderRadius={"15px"}
-                        padding={"0px 10px 0px 10px"}
-                      >
-                        OFFERS AVAILABLE
-                      </Badge>
-                      <Badge
-                        variant="outline"
-                        colorScheme="green"
-                        borderRadius={"15px"}
-                        padding={"0px 10px 0px 10px"}
-                      >
-                        CASHBACK
-                      </Badge>
-                    </Stack>
+                        {product.mrp}{" "}
+                      </s>{" "}
+                    </Text>
+                    <Text
+                      color="green.600"
+                      fontSize="x-small"
+                      fontWeight={"bold"}
+                    >
+                      {" "}
+                      <span>&#8377;</span>
+                      {product.discount}
+                    </Text>
                   </Stack>
-                </CardBody>
-                <Divider />
-                <br />
-                <CardFooter
-                  justify="space-between"
-                  alignItems={"center"}
-                  height={"20px"}
-                  mt="-4"
-                >
-                  <Button flex="1" variant="ghost">
-                    {
-                      <Stack direction="row" align={"center"} fontWeight={""}>
-                        <input
-                          type="checkbox"
-                          name="compare-btn"
-                          id="compare-btn"
-                        />
-                        <label>Compare</label>
-                      </Stack>
-                    }
-                  </Button>
-                  <Button flex="1" variant="ghost" leftIcon={<BsHeart />}>
-                    Wishlist
-                  </Button>
-                </CardFooter>
-              </Card>
-            ))}
-          </div>
+                  <Stack direction="row">
+                    <Badge
+                      variant="outline"
+                      colorScheme="green"
+                      borderRadius={"15px"}
+                      padding={"0px 10px 0px 10px"}
+                    >
+                      OFFERS AVAILABLE
+                    </Badge>
+                    <Badge
+                      variant="outline"
+                      colorScheme="green"
+                      borderRadius={"15px"}
+                      padding={"0px 10px 0px 10px"}
+                    >
+                      CASHBACK
+                    </Badge>
+                  </Stack>
+                </Stack>
+              </CardBody>
+              <Divider />
+              <br />
+              <CardFooter
+                justify="space-between"
+                alignItems={"center"}
+                height={"20px"}
+                mt="-4"
+              >
+                <Button flex="1" variant="ghost">
+                  {
+                    <Stack direction="row" align={"center"} fontWeight={""}>
+                      <input
+                        type="checkbox"
+                        name="compare-btn"
+                        id="compare-btn"
+                      />
+                      <label data-cy="product-add-item-to-cart-button" onClick={() => {
+                        let obj = {}
+                        for(let i of data)
+                          if(i.id === product.id){
+                            obj = i
+                          }
+                        dispatch(addItemCart(obj));
+
+                      }}>Add to Cart</label>
+                    </Stack>
+                  }
+                </Button>
+                <Button flex="1" variant="ghost" leftIcon={<BsHeart />}>
+                  Wishlist
+                </Button>
+              </CardFooter>
+            </Card>
+          ))}
+                
+        </div>)}
         </div>
       </div>
     </>
