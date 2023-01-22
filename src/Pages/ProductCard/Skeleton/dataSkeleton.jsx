3927acaad53542657e3aeea7ef9af167ff62f18d
unknown <vermabraj>
2023-01-22 16:09:32 +0530

    added skeleton

--- src/Pages/ProductCard/Skeleton/dataSkeleton.jsx
+++ src/Pages/ProductCard/Skeleton/dataSkeleton.jsx
@@ -3,8 +3,10 @@ import Skeleton from 'react-loading-skeleton'
 import 'react-loading-skeleton/dist/skeleton.css'
 
 const dataSkeleton = () => {
+  //bm
   return (
     <div id="productCards">
+
             <div>
             <Skeleton height={140} />
           <h1><Skeleton/></h1>
